277e07e190aa7add9083a327980f2694ee77bff2
unknown <user@mil8a-202a-05.mmcs.sfedu.ru>
2018-09-11 17:04:31 +0300

    add picture

---
+++ Module1/Task2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+                MessageBox.Show(sr.ReadToEnd());
+                sr.Close();
+            }
+            pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            pictureBox1.Size = pictureBox1.Image.Size;
+        }
+       
+    }
+}
