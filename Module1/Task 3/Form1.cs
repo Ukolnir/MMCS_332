100d57de7b61482fbb37525e0cf021d4b53e6e90
Ukolnir <metallwind@gmail.com>
2018-09-11 19:28:56 +0300

    upd task3 - Ukolnir
    Прост настроила у себя проект, чтоб заработало все

--- Module1/Task 3/Form1.cs
+++ Module1/Task 3/Form1.cs
@@ -10,59 +10,88 @@ using System.Windows.Forms;
 
 namespace task3
 {
-	public partial class Form1 : Form
-	{
-		byte[] rgbValues;
-		Bitmap bmp;
+    public partial class Form1 : Form
+    {
+        byte[] rgbValues;
+        Bitmap bmp;
 
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-		public Form1()
-		{
-			InitializeComponent();
-		}
+        private void openPicture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            //openFileDialog1.Filter = "Cursor Files|*.cur";
+            //openFileDialog1.Title = "Select a Cursor File";
 
-		private void openPicture_Click(object sender, EventArgs e)
-		{
-			OpenFileDialog openFileDialog1 = new OpenFileDialog();
-			//openFileDialog1.Filter = "Cursor Files|*.cur";
-			//openFileDialog1.Title = "Select a Cursor File";
- 
-			if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-			{  
-				bmp = new Bitmap(openFileDialog1.FileName);
-				pictureBox1.Image = bmp;
-			}
-		}
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                bmp = new Bitmap(openFileDialog1.FileName);
+                rGBHSL();
+                pictureBox1.Image = bmp;
+            }
+        }
 
-		private void rGBHSLToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			Bitmap bmp = pictureBox1.Image as Bitmap;
-			// Lock the bitmap's bits.  
-			Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
-			System.Drawing.Imaging.BitmapData bmpData =
-				bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
-				bmp.PixelFormat);
+        private void rGBHSLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Lock the bitmap's bits.  
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            System.Drawing.Imaging.BitmapData bmpData =
+                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                bmp.PixelFormat);
 
-			// Get the address of the first line.
-			IntPtr ptr = bmpData.Scan0;
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
 
-			// Declare an array to hold the bytes of the bitmap.
-			int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
-			rgbValues = new byte[bytes];
+            // Declare an array to hold the bytes of the bitmap.
+            int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+            rgbValues = new byte[bytes];
 
-			// Copy the RGB values into the array.
-			System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+            // Copy the RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
 
-			// Set every third value to 255. A 24bpp bitmap will look red.  
-			for (int counter = 0; counter < rgbValues.Length; counter += 3)
-			{
-				rgbValues[counter+1] = 255;                                
-			}
-			// Copy the RGB values back to the bitmap
-			System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+            // Set every third value to 255. A 24bpp bitmap will look red.  
+            for (int counter = 0; counter < rgbValues.Length; counter += 3)
+            {
+                rgbValues[counter + 1] = 255;
+            }
+            // Copy the RGB values back to the bitmap
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
 
-			// Unlock the bits.
-			bmp.UnlockBits(bmpData);
-		}
-	}
+            // Unlock the bits.
+            bmp.UnlockBits(bmpData);
+        }
+
+        private void rGBHSL()
+        {
+            // Lock the bitmap's bits.  
+            Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            System.Drawing.Imaging.BitmapData bmpData =
+                bmp.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                bmp.PixelFormat);
+
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Declare an array to hold the bytes of the bitmap.
+            int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+            rgbValues = new byte[bytes];
+
+            // Copy the RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            // Set every third value to 255. A 24bpp bitmap will look red.  
+            for (int counter = 0; counter < rgbValues.Length; counter += 3)
+            {
+                rgbValues[counter + 1] = 255;
+            }
+            // Copy the RGB values back to the bitmap
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            // Unlock the bits.
+            bmp.UnlockBits(bmpData);
+        }
+    }
 }
