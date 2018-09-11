7438be4fe942a71f6734242b34bb53ebcc383de1
unknown <user@mil8a-202a-05.mmcs.sfedu.ru>
2018-09-11 17:35:01 +0300

    task2 complete

--- Module1/Task2/Form1.Designer.cs
+++ Module1/Task2/Form1.Designer.cs
@@ -31,7 +31,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -52,20 +58,50 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(24, 125);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(220, 192);
+            this.pictureBox1.Size = new System.Drawing.Size(331, 296);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(361, 125);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(331, 296);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(361, 427);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(331, 296);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(24, 427);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(331, 296);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(580, 423);
+            this.ClientSize = new System.Drawing.Size(715, 740);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -75,6 +111,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
 
--- Module1/Task2/Form1.cs
+++ Module1/Task2/Form1.cs
@@ -17,17 +17,62 @@ namespace Task2
             InitializeComponent();
         }
 
+        private static Bitmap image2, image3, image4;
         private void button1_Click(object sender, EventArgs e)
         {
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 System.IO.StreamReader sr = new
-                   System.IO.StreamReader(openFileDialog1.FileName);
+                   System.IO.StreamReader(openFileDialog1.FileName, Encoding.Default);
                 MessageBox.Show(sr.ReadToEnd());
                 sr.Close();
             }
             pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
-            pictureBox1.Size = pictureBox1.Image.Size;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+            image2 = new Bitmap(openFileDialog1.FileName, true);
+            pictureBox2.Image = image2;
+            image3 = new Bitmap(openFileDialog1.FileName, true);
+            pictureBox3.Image = image3;
+            image4 = new Bitmap(openFileDialog1.FileName, true);
+            pictureBox4.Image = image4;
+
+            for (int x = 0; x < image2.Width; x++)
+            {
+                for (int y = 0; y < image2.Height; y++)
+                {
+                    Color pixelColor = image2.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                    image2.SetPixel(x, y, newColor);
+                }
+            }
+
+            for (int x = 0; x < image3.Width; x++)
+            {
+                for (int y = 0; y < image3.Height; y++)
+                {
+                    Color pixelColor = image3.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(0, pixelColor.G, 0);
+                    image3.SetPixel(x, y, newColor);
+                }
+            }
+
+            for (int x = 0; x < image4.Width; x++)
+            {
+                for (int y = 0; y < image4.Height; y++)
+                {
+                    Color pixelColor = image4.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(0, 0, pixelColor.B);
+                    image4.SetPixel(x, y, newColor);
+                }
+            }
+
+
+
         }
        
     }
