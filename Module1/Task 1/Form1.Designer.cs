839c3f43b2bc91a609bea93155206af4232da137
Kulishova Alexandra <sasha_kulishova@mail.ru>
2018-09-15 22:31:29 +0300

    add commits

--- Module1/Task 1/Form1.Designer.cs
+++ Module1/Task 1/Form1.Designer.cs
@@ -28,10 +28,10 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -109,12 +109,13 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
             this.button1.Location = new System.Drawing.Point(12, 115);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(73, 64);
             this.button1.TabIndex = 6;
             this.button1.Text = "Открыть файл";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox4
@@ -128,20 +129,20 @@
             // 
             // chart1
             // 
-            chartArea4.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.chart1.Legends.Add(legend4);
-            this.chart1.Location = new System.Drawing.Point(428, 3);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(438, 2);
             this.chart1.Name = "chart1";
-            series7.ChartArea = "ChartArea1";
-            series7.Legend = "Legend1";
-            series7.Name = "image 1";
-            series8.ChartArea = "ChartArea1";
-            series8.Legend = "Legend1";
-            series8.Name = "image 2";
-            this.chart1.Series.Add(series7);
-            this.chart1.Series.Add(series8);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "image 1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "image 2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(571, 300);
             this.chart1.TabIndex = 8;
             this.chart1.Text = "chart1";
@@ -150,7 +151,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1011, 577);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1011, 584);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.button1);
--- Module1/Task 1/Form1.cs
+++ Module1/Task 1/Form1.cs
@@ -18,21 +18,24 @@ namespace Task1
 		}
 
         OpenFileDialog open_dialog;
-        List<int> l1, l2;
+        List<int> l1, l2; //для хранения кол-во пикселей с оттенкам серого в 1ом и 2м изображении (для гистограммы)
 
-        private void simpleGrey(Bitmap bmp)
+
+        //оттенки серого (Y = 0.333R + 0.333G + 0.333B)
+        private void simpleGrey(Bitmap bmp) 
 		{
 			pictureBox2.Image = bmp;
 			pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
 
+            //проходимся по пикселям, изменяя цвет
             for (int i = 0; i < bmp.Width; ++i)
                 for (int j = 0; j < bmp.Height; ++j)
                 {
-                    Color pixelColor = bmp.GetPixel(i, j);
-                    int c = (int)(((float)pixelColor.R + (float)pixelColor.B + (float)pixelColor.G) / 3.0f);
-                    l1[c]++; 
+                    Color pixelColor = bmp.GetPixel(i, j); // получаем пиксель исходного ихображения
+                    int c = (int)(((float)pixelColor.R + (float)pixelColor.B + (float)pixelColor.G) / 3.0f); // расчитываем оттенок серого
+                    l1[c]++; //увеличиваем число пикселей данного оттенка на 1 в контейнере
                     Color newColor = Color.FromArgb(c, c, c);
-                    bmp.SetPixel(i, j, newColor);
+                    bmp.SetPixel(i, j, newColor); //устанавливаем новый цвет пикселя
                 }
         }
 
@@ -42,17 +45,20 @@ namespace Task1
             pictureBox3.Image = bmp;
             pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
 
+            //проходимся по пикселям, изменяя цвет
             for (int i = 0; i < bmp.Width; ++i)
                 for (int j = 0; j < bmp.Height; ++j)
                 {
-                    Color pixelColor = bmp.GetPixel(i, j);
-                    int c = (int)(0.2126 * pixelColor.R + 0.7152 * pixelColor.G + 0.0722 * pixelColor.B);
-                    l2[c]++;
+                    Color pixelColor = bmp.GetPixel(i, j); // получаем пиксель исходного ихображения
+                    int c = (int)(0.2126 * pixelColor.R + 0.7152 * pixelColor.G + 0.0722 * pixelColor.B); // расчитываем оттенок серого
+                    l2[c]++; //увеличиваем число пикселей данного оттенка на 1 в контейнере
                     Color newColor = Color.FromArgb(c, c, c);
-                    bmp.SetPixel(i, j, newColor);
+                    bmp.SetPixel(i, j, newColor); //устанавливаем новый цвет пикселя
                 }
         }
 
+
+        //разность двух полученных полутоновых изображений
         private void imageDifference(Bitmap bmp1, Bitmap bmp2)
         {
             Bitmap bmp3 = (Bitmap)bmp2.Clone();
@@ -66,25 +72,26 @@ namespace Task1
                     Color pixC2 = bmp2.GetPixel(i, j);
                     int d1 = pixC2.R - pixC1.R + pixC2.G - pixC1.G + pixC2.B - pixC1.B;
                     int d2 = pixC1.R - pixC2.R + pixC1.G - pixC2.G + pixC1.B - pixC2.B;
-                    byte d = (byte)Math.Max(d1, d2);
+                    byte d = (byte)Math.Max(d1, d2); //выбираем максимум из двух сумм, т.к. значение d1 или d2 м.б. отрицательным
                     Color newColor = Color.FromArgb(d, d, d);
                     bmp3.SetPixel(i, j, newColor);
                 }
-
         }
 
+        //построение гистограммы
         private void Histogram()
         {
             
-            chart1.Series["image 1"].Points.Clear();
-            chart1.Series["image 2"].Points.Clear();
+            chart1.Series["image 1"].Points.Clear(); //интенсивность (оттенки серого) первого изображения
+            chart1.Series["image 2"].Points.Clear(); //интенсивность (оттенки серого) второго изображения
 
+            //строим гистограмму по кол-ву пикселей оттенков серого (от 0 до 255) 1го и 2го изображений
             for (int i = 0; i < 256; ++i)
             {
                 chart1.Series["image 1"].Points.AddY(l1[i]);
-                chart1.Series["image 1"].Points[i].Color = Color.Blue;
+                chart1.Series["image 1"].Points[i].Color = Color.Blue; //цвет столбцов, относящихся к 1му изображению
                 chart1.Series["image 2"].Points.AddY(l2[i]);
-                chart1.Series["image 2"].Points[i].Color = Color.Orange;
+                chart1.Series["image 2"].Points[i].Color = Color.Orange; //цвет столбцов, относящихся к 2му изображению
             }
             chart1.Update();
         }
@@ -94,37 +101,29 @@ namespace Task1
 
 			if (res == DialogResult.OK) //если в окне была нажата кнопка "ОК"
 			{
-				try
-				{
-					Bitmap bmp = new Bitmap(open_dialog.FileName);
+				
+			    Bitmap bmp = new Bitmap(open_dialog.FileName); 
                     
 
-					pictureBox1.Image = bmp;
-					pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-                    Bitmap bmp1 = (Bitmap)bmp.Clone();
-                    Bitmap bmp2 = (Bitmap)bmp.Clone();
+			    pictureBox1.Image = bmp;
+			    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                Bitmap bmp1 = (Bitmap)bmp.Clone(); //создаем копию исх-го изображения для simpleGrey
+                Bitmap bmp2 = (Bitmap)bmp.Clone(); //создаем копию исх-го изображения для intensiveGrey
 
-                    l1 = new List<int>();
-                    l2 = new List<int>();
+                l1 = new List<int>(); //инициализируем контейнеры для последующего построения гистограммы
+                l2 = new List<int>();
  
-                    for (int i = 0; i < 256; ++i)
-                    {
-                        l1.Add(0);
-                        l2.Add(0);
-                    }
-
-                    simpleGrey(bmp1);
-                    intensiveGrey(bmp2);
-                    imageDifference((Bitmap)bmp1.Clone(), (Bitmap)bmp2.Clone());
-                    Histogram();
+                for (int i = 0; i < 256; ++i)
+                {
+                    l1.Add(0);
+                    l2.Add(0);
                 }
-				catch
-				{
-					DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
-					"Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				}
 
-                
+                simpleGrey(bmp1);
+                intensiveGrey(bmp2);
+                imageDifference((Bitmap)bmp1.Clone(), (Bitmap)bmp2.Clone());
+                Histogram();
+           
             }
 		}
 
