10ccdbfd7d84d7ec657e090c9a544781c9feb159
unknown <user@mil8a-202a-04.mmcs.sfedu.ru>
2018-09-25 17:58:42 +0300

    create drawing in Task 1b

--- Module2/Task 1b/Task 1b/Form1.Designer.cs
+++ Module2/Task 1b/Task 1b/Form1.Designer.cs
@@ -31,6 +31,10 @@
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.pictureBox = new System.Windows.Forms.PictureBox();
 			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
 			this.SuspendLayout();
@@ -67,11 +71,52 @@
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 166);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(170, 45);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Choose file";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(12, 229);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(170, 51);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Change mode";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 296);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(73, 13);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Current mode:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(63, 309);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(0, 13);
+			this.label2.TabIndex = 6;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(729, 467);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.pictureBox);
 			this.Controls.Add(this.pictureBox1);
@@ -80,6 +125,7 @@
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -88,6 +134,10 @@
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.PictureBox pictureBox;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
 	}
 }
 
--- Module2/Task 1b/Task 1b/Form1.cs
+++ Module2/Task 1b/Task 1b/Form1.cs
@@ -11,26 +11,35 @@ using System.Windows.Forms;
 namespace Task_1b
 {
 	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-
-		private Point start;
+	{	private Point start;
 		private bool drawing = false;
 		private Image orig;
 		Color c;
+		OpenFileDialog open_dialog;
+		bool f;
+
+		public Form1()
+		{
+			InitializeComponent();
+			f = false;
+			label2.Text = "Drawing";
+		}
+
+	
 
 		private void pictureBox_Click(object sender, EventArgs e)
 		{
 			
 		}
+
 		private void pictureBox_MouseDown(object sender, MouseEventArgs e)
 		{
-			start = new Point(e.X, e.Y);
-			orig = pictureBox.Image;
-			drawing = true;
+			if (!f)
+			{
+				start = new Point(e.X, e.Y);
+				orig = pictureBox.Image;
+				drawing = true;
+			}
 		}
 
 		private void pictureBox_MouseMove(object sender, MouseEventArgs e)
@@ -54,6 +63,23 @@ namespace Task_1b
 		{
 			var g = Graphics.FromHwnd(pictureBox.Handle);
 			g.Clear(pictureBox.BackColor);
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			open_dialog = new OpenFileDialog(); //создание диалогового окна для выбора файла
+			open_dialog.Filter = "Image Files(*.BMP;*.JPG;**.PNG)|*.BMP;*.JPG;**.PNG|All files (*.*)|*.*"; //формат загружаемого файла
+			DialogResult dr = open_dialog.ShowDialog();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			if (!f)
+				label2.Text = "Filling";
+			else
+				label2.Text = "Drawing";
+
+			f = !f;
 		}
 	}
 }
