9b43e7610937a51b368630f20533a8718d426ea7
BlueMadWolf <sasha_kulishova@mail.ru>
2018-09-29 19:45:27 +0300

    Task 1b was completed

--- Module2/Task 1b/Task 1b/Form1.Designer.cs
+++ Module2/Task 1b/Task 1b/Form1.Designer.cs
@@ -28,104 +28,109 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.pictureBox = new System.Windows.Forms.PictureBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(170, 138);
-			this.pictureBox1.TabIndex = 0;
-			this.pictureBox1.TabStop = false;
-			// 
-			// pictureBox
-			// 
-			this.pictureBox.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-			this.pictureBox.Location = new System.Drawing.Point(210, 12);
-			this.pictureBox.Name = "pictureBox";
-			this.pictureBox.Size = new System.Drawing.Size(491, 427);
-			this.pictureBox.TabIndex = 1;
-			this.pictureBox.TabStop = false;
-			this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
-			this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
-			this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
-			this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(12, 396);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(170, 43);
-			this.button1.TabIndex = 2;
-			this.button1.Text = "Clean";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(12, 166);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(170, 45);
-			this.button2.TabIndex = 3;
-			this.button2.Text = "Choose file";
-			this.button2.UseVisualStyleBackColor = true;
-			this.button2.Click += new System.EventHandler(this.button2_Click);
-			// 
-			// button3
-			// 
-			this.button3.Location = new System.Drawing.Point(12, 229);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(170, 51);
-			this.button3.TabIndex = 4;
-			this.button3.Text = "Change mode";
-			this.button3.UseVisualStyleBackColor = true;
-			this.button3.Click += new System.EventHandler(this.button3_Click);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 296);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(73, 13);
-			this.label1.TabIndex = 5;
-			this.label1.Text = "Current mode:";
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(63, 309);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(0, 13);
-			this.label2.TabIndex = 6;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(729, 467);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.pictureBox);
-			this.Controls.Add(this.pictureBox1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(170, 138);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pictureBox.Location = new System.Drawing.Point(210, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(491, 427);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 74);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clean";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 76);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Choose file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 267);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Current mode:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 299);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Drawing";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton1_MouseClick);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 322);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(51, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Filling";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton2_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 467);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -135,9 +140,9 @@
 		private System.Windows.Forms.PictureBox pictureBox;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.Button button3;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
-	}
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
 }
 
--- Module2/Task 1b/Task 1b/Form1.cs
+++ Module2/Task 1b/Task 1b/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Task_1b
 {
@@ -16,30 +17,76 @@ namespace Task_1b
 		private Image orig;
 		Color c;
 		OpenFileDialog open_dialog;
-		bool f;
+        Bitmap back;
 
 		public Form1()
 		{
-			InitializeComponent();
-			f = false;
-			label2.Text = "Drawing";
-		}
+			InitializeComponent();;
+            radioButton1.Checked = true;
 
-	
 
-		private void pictureBox_Click(object sender, EventArgs e)
-		{
-			
-		}
+            //создаем фон
+            Bitmap b = new Bitmap(pictureBox.Width, pictureBox.Height);
+            pictureBox.Image = b;
+            Clear(); // очищаем pictureBox
+            
+        }
+
+        //поиск границ
+        private void find_borders(Point our_p, ref Point left_b, ref Point right_b, Bitmap b, Color c)
+        {
+            while (left_b.X > 0 && equalColors(b.GetPixel(left_b.X, left_b.Y), c))
+            {
+                left_b.X -= 1;
+            }
+
+            while (right_b.X < b.Width && equalColors(b.GetPixel(right_b.X, right_b.Y), c))
+                right_b.X += 1;
+        }
+
+        //проверяем цвета на равенство
+        private bool equalColors(Color c1, Color c2)
+        {
+                return c1.R == c2.R && c1.G == c2.G && c1.B == c2.B;
+        }
+
+        //заливка
+        private void filling(Point p, Color c)
+        {
+            Bitmap b = (Bitmap)pictureBox.Image;
+
+            //если пиксель еще не был закрашен
+            if (0 < p.X && p.X < b.Width && 0 < p.Y && p.Y < b.Height && equalColors(b.GetPixel(p.X, p.Y), c))
+            {
+                var g = Graphics.FromImage(b);
+                Point left_b = p, right_b = p;
+                find_borders(p, ref left_b, ref right_b, b, c); //поиск границ
+                Rectangle r = new Rectangle(left_b.X + 1, p.Y, right_b.X - left_b.X - 1, 1); 
+                Bitmap line = back.Clone(r, back.PixelFormat); //копируем линию из заданного изображения
+
+                g.DrawImage(line, r);
+                pictureBox.Image = b;
+                
+                for (int i = left_b.X + 1; i < right_b.X; ++i)
+                        filling(new Point(i, p.Y + 1), c);
+
+                for (int i = left_b.X + 1; i < right_b.X; ++i)
+                        filling(new Point(i, p.Y - 1), c);
+            }
+        }
 
 		private void pictureBox_MouseDown(object sender, MouseEventArgs e)
 		{
-			if (!f)
-			{
-				start = new Point(e.X, e.Y);
-				orig = pictureBox.Image;
-				drawing = true;
-			}
+            start = new Point(e.X, e.Y);
+            if (radioButton1.Checked) //рисуем
+            {
+                orig = pictureBox.Image;
+                drawing = true;
+            }
+            else
+            {
+                filling(start, pictureBox.BackColor); // заливаем
+            }
 		}
 
 		private void pictureBox_MouseMove(object sender, MouseEventArgs e)
@@ -47,9 +94,12 @@ namespace Task_1b
 			if (!drawing) return;
 			var finish = new Point(e.X, e.Y);
 			var pen = new Pen(Color.Black, 1f);
-			var g = Graphics.FromHwnd(pictureBox.Handle);
+            
+			var g = Graphics.FromImage(pictureBox.Image); 
 			g.DrawLine(pen, start, finish);
+            pen.Dispose();
 			g.Dispose();
+            pictureBox.Image = pictureBox.Image;
 			pictureBox1.Invalidate();
 			start = finish;
 		}
@@ -57,12 +107,18 @@ namespace Task_1b
 		private void pictureBox_MouseUp(object sender, MouseEventArgs e)
 		{
 			drawing = false;
-		}
+        }
+
+        private void Clear()
+        {
+            var g = Graphics.FromImage(pictureBox.Image);
+            g.Clear(pictureBox.BackColor);
+            pictureBox.Image = pictureBox.Image;
+        }
 
 		private void button1_Click(object sender, EventArgs e)
 		{
-			var g = Graphics.FromHwnd(pictureBox.Handle);
-			g.Clear(pictureBox.BackColor);
+            Clear();
 		}
 
 		private void button2_Click(object sender, EventArgs e)
@@ -70,16 +126,31 @@ namespace Task_1b
 			open_dialog = new OpenFileDialog(); //создание диалогового окна для выбора файла
 			open_dialog.Filter = "Image Files(*.BMP;*.JPG;**.PNG)|*.BMP;*.JPG;**.PNG|All files (*.*)|*.*"; //формат загружаемого файла
 			DialogResult dr = open_dialog.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                Bitmap b = new Bitmap(open_dialog.FileName);
+                back = new Bitmap(b, pictureBox.Size);
+                pictureBox1.Image = new Bitmap(b, pictureBox1.Size);
+            }
 		}
 
-		private void button3_Click(object sender, EventArgs e)
-		{
-			if (!f)
-				label2.Text = "Filling";
-			else
-				label2.Text = "Drawing";
 
-			f = !f;
-		}
-	}
+        private void radioButton1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                radioButton1.Checked = true;
+                radioButton2.Checked = false;
+            }
+        }
+
+        private void radioButton2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                radioButton2.Checked = true;
+                radioButton1.Checked = false;
+            }
+        }
+    }
 }
