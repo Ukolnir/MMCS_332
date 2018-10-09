b6eca662b7b13657b2968ec40751fcd156ce4021
unknown <user@mil8a-202a-05.mmcs.sfedu.ru>
2018-10-09 17:58:04 +0300

    task3 updated

---
+++ Module4/Task 3/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Task_3
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.radioAdd = new System.Windows.Forms.RadioButton();
+			this.radioDelete = new System.Windows.Forms.RadioButton();
+			this.radioMove = new System.Windows.Forms.RadioButton();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.pictureBox1.Location = new System.Drawing.Point(227, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(568, 502);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+			this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+			this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "label1";
+			// 
+			// radioAdd
+			// 
+			this.radioAdd.AutoSize = true;
+			this.radioAdd.Location = new System.Drawing.Point(15, 45);
+			this.radioAdd.Name = "radioAdd";
+			this.radioAdd.Size = new System.Drawing.Size(119, 17);
+			this.radioAdd.TabIndex = 2;
+			this.radioAdd.TabStop = true;
+			this.radioAdd.Text = "Добавление точки";
+			this.radioAdd.UseVisualStyleBackColor = true;
+			// 
+			// radioDelete
+			// 
+			this.radioDelete.AutoSize = true;
+			this.radioDelete.Location = new System.Drawing.Point(15, 68);
+			this.radioDelete.Name = "radioDelete";
+			this.radioDelete.Size = new System.Drawing.Size(106, 17);
+			this.radioDelete.TabIndex = 3;
+			this.radioDelete.TabStop = true;
+			this.radioDelete.Text = "Удаление точки";
+			this.radioDelete.UseVisualStyleBackColor = true;
+			// 
+			// radioMove
+			// 
+			this.radioMove.AutoSize = true;
+			this.radioMove.Location = new System.Drawing.Point(15, 91);
+			this.radioMove.Name = "radioMove";
+			this.radioMove.Size = new System.Drawing.Size(129, 17);
+			this.radioMove.TabIndex = 4;
+			this.radioMove.TabStop = true;
+			this.radioMove.Text = "Перемещение точки";
+			this.radioMove.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(807, 526);
+			this.Controls.Add(this.radioMove);
+			this.Controls.Add(this.radioDelete);
+			this.Controls.Add(this.radioAdd);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RadioButton radioAdd;
+		private System.Windows.Forms.RadioButton radioDelete;
+		private System.Windows.Forms.RadioButton radioMove;
+	}
+}
+
---
+++ Module4/Task 3/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_3
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+			g = Graphics.FromImage(pictureBox1.Image);
+			bmp = (Bitmap)pictureBox1.Image;
+		}
+
+		private static List<Point> points = new List<Point>();
+		private static Graphics g;
+		private static Bitmap bmp;
+
+		private void drawPoint(int x, int y, Color c)
+		{
+			bmp.SetPixel(x, y, c);
+
+			bmp.SetPixel(x + 1, y, c);
+			bmp.SetPixel(x - 1, y, c);
+			bmp.SetPixel(x, y + 1, c);
+			bmp.SetPixel(x, y - 1, c);
+
+			bmp.SetPixel(x + 1, y + 1, c);
+			bmp.SetPixel(x - 1, y + 1, c);
+			bmp.SetPixel(x + 1, y - 1, c);
+			bmp.SetPixel(x - 1, y - 1, c);
+		}
+
+		private bool addPoint(int x, int y)
+		{
+			if (x > 0 && x < pictureBox1.Width && y > 0 && y < pictureBox1.Height)
+			{
+				points.Add(new Point(x, y));
+
+				drawPoint(x, y, Color.Black);
+
+				label1.Text = "Point added";
+				pictureBox1.Image = bmp;
+
+				return true;
+			}
+			else
+			{
+				label1.Text = "Point abroad borders";
+
+				return false;
+			}
+		}
+
+		private bool deletePoint(int x, int y)
+		{
+			if (x > 0 && x < pictureBox1.Width && y > 0 && y < pictureBox1.Height)
+			{
+				Point p;
+				int diff = 7;
+				if (points.Exists(point => ((point.X > x - diff) && (point.X < x + diff)) &&
+					((point.Y > y - diff) && (point.Y < y + diff))))
+				{
+					p = points.Find(point => ((point.X > x - diff) && (point.X < x + diff)) &&
+						((point.Y > y - diff) && (point.Y < y + diff)));
+					points.Remove(p);
+
+					drawPoint(p.X, p.Y, pictureBox1.BackColor);
+
+					label1.Text = "Point deleted";
+					pictureBox1.Image = bmp;
+
+					return true;
+				}
+				else
+				{
+					label1.Text = "Point not found";
+
+					return false;
+				}
+			}
+			else
+			{
+				label1.Text = "Point abroad borders";
+
+				return false;
+			}
+		}
+
+		private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+		{
+			int x = e.Location.X;
+			int y = e.Location.Y;
+			if (radioAdd.Checked)
+			{
+				addPoint(x, y);
+			}
+			if (radioDelete.Checked)
+			{
+				deletePoint(x, y);
+			}
+		}
+
+		private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+		{
+			int x = e.Location.X;
+			int y = e.Location.Y;
+
+			if (radioMove.Checked)
+			{
+				deletePoint(x, y);
+			}
+		}
+
+		private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+		{
+			int x = e.Location.X;
+			int y = e.Location.Y;
+
+			if (radioMove.Checked)
+			{
+				if (addPoint(x, y))
+				{
+					label1.Text = "Point moved";
+				}
+			}
+		}
+	}
+}
