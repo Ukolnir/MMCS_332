c25932c36e2f7530fc8ea88ffe7f281cac5ca416
Ukolnir <metallwind@gmail.com>
2018-10-13 23:37:28 +0300

    Upd task1
    Осталось реализовать скобочные L-системы

--- Module4/Task 1/Form1.Designer.cs
+++ Module4/Task 1/Form1.Designer.cs
@@ -28,86 +28,98 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-			this.загрузитьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.label1 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-			this.menuStrip1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// pictureBox1
-			// 
-			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.pictureBox1.Location = new System.Drawing.Point(12, 38);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(761, 369);
-			this.pictureBox1.TabIndex = 0;
-			this.pictureBox1.TabStop = false;
-			// 
-			// menuStrip1
-			// 
-			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.загрузитьФайлToolStripMenuItem});
-			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(796, 24);
-			this.menuStrip1.TabIndex = 1;
-			this.menuStrip1.Text = "menuStrip1";
-			// 
-			// загрузитьФайлToolStripMenuItem
-			// 
-			this.загрузитьФайлToolStripMenuItem.Name = "загрузитьФайлToolStripMenuItem";
-			this.загрузитьФайлToolStripMenuItem.Size = new System.Drawing.Size(105, 20);
-			this.загрузитьФайлToolStripMenuItem.Text = "Загрузить файл";
-			this.загрузитьФайлToolStripMenuItem.Click += new System.EventHandler(this.loadFileToolStripMenuItem_Click);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(23, 434);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(119, 13);
-			this.label1.TabIndex = 2;
-			this.label1.Text = "Количество итераций:";
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(148, 431);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(58, 20);
-			this.textBox1.TabIndex = 3;
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(513, 418);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(160, 44);
-			this.button1.TabIndex = 4;
-			this.button1.Text = "Применить";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(796, 498);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.pictureBox1);
-			this.Controls.Add(this.menuStrip1);
-			this.MainMenuStrip = this.menuStrip1;
-			this.Name = "Form1";
-			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-			this.menuStrip1.ResumeLayout(false);
-			this.menuStrip1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.загрузитьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(947, 518);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьФайлToolStripMenuItem,
+            this.очиститьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(971, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // загрузитьФайлToolStripMenuItem
+            // 
+            this.загрузитьФайлToolStripMenuItem.Name = "загрузитьФайлToolStripMenuItem";
+            this.загрузитьФайлToolStripMenuItem.Size = new System.Drawing.Size(105, 20);
+            this.загрузитьФайлToolStripMenuItem.Text = "Загрузить файл";
+            this.загрузитьФайлToolStripMenuItem.Click += new System.EventHandler(this.loadFileToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 605);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество итераций:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 602);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(141, 24);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(623, 574);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 646);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -119,6 +131,7 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
 	}
 }
 
--- Module4/Task 1/Form1.cs
+++ Module4/Task 1/Form1.cs
@@ -8,68 +8,156 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Task_1
-{
-	public partial class Form1 : Form
-	{
-		Bitmap bmp;
-		Graphics g;
+namespace Task_1{
+    public partial class Form1 : Form{
+        Bitmap bmp;
+        Graphics g;
+        public Form1(){
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bmp = (Bitmap)pictureBox1.Image;
+            Clear();
+            //g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+        }
 
-		List<string> param;
-		int cnt;
+        private void Clear(){
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = pictureBox1.Image;
+        }
 
+        List<string> param;
 
-		public Form1()
-		{
-			InitializeComponent();
-			pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-			bmp = (Bitmap)pictureBox1.Image;
-			Clear();
-			//g = Graphics.FromImage(bmp);
-			pictureBox1.Image = bmp;
-		}
+        private void loadFileToolStripMenuItem_Click(object sender, EventArgs e){
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files(*.txt)|*.txt|All files (*.*)|*.*"; //формат загружаемого файла
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK){
+                param = new List<string>();
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog.FileName, Encoding.Default)){
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                        param.Add(line);
+                }
+            }
+        }
 
-		private void Clear()
-		{
-			g = Graphics.FromImage(pictureBox1.Image);
-			g.Clear(pictureBox1.BackColor);
-			pictureBox1.Image = pictureBox1.Image;
-		}
+        Dictionary<char, string> rules;
+        string axiom;
+        int cnt;
+        double angle;
+        char direction;
 
-		private void loadFileToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-			openFileDialog.Filter = "Text Files(*.txt)|*.txt|All files (*.*)|*.*"; //формат загружаемого файла
-			if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-			{
-				param = new List<string>();
-				using (System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog.FileName, Encoding.Default))
-				{
-					string line;
-					while ((line = sr.ReadLine()) != null)
-					{
-						param.Add(line);
-					}
-				}
-			}
-		}
+        List<Point> fractal;
 
-		public List<char> parse_start(string s) {
-			List<char> res = new List<char>();
-			for (int i = 0; i < s.Length; ++i)
-				res.Add(s[i]);
-			return res;
-		}
+        private double[,] matrix_multiplication(double[,] m1, double[,] m2){
+            double[,] res = new double[m1.GetLength(0), m2.GetLength(1)];
+            for (int i = 0; i < m1.GetLength(0); ++i)
+                for (int j = 0; j < m2.GetLength(1); ++j)
+                    for (int k = 0; k < m2.GetLength(0); k++)
+                        res[i, j] += m1[i, k] * m2[k, j];
+            return res;
+        }
 
-		private void button1_Click(object sender, EventArgs e)
-		{
+        private Point rotation(double ang, int x0, int y0, int x, int y) {
+            double p = ang * Math.PI / 180;
+            double cos = Math.Cos(p);
+            double sin = Math.Sin(p);
+            double[,] transferalMatrix = new double[,] { {cos, sin, 0}, {-sin, cos, 0}, 
+                        {cos*(-x0)+y0*sin+x0, (-x0)*sin-y0*cos+y0, 1}};
+            double[,] point = new double[,] { { x, y, 1.0 } };
+            double[,] res = matrix_multiplication(point, transferalMatrix);
+            return new Point(Convert.ToInt32(res[0, 0]), Convert.ToInt32(res[0, 1]));
+        }
 
-			cnt = Convert.ToInt32(textBox1.Text);
-			List<char> starts = parse_start(param[1]);
+        private void perform_fractal(string str) { 
+            int len = 10;
+            int ht = pictureBox1.Height;
+            fractal = new List<Point>();
+            fractal.Add(new Point(0, ht));
 
+            double ang = 0;
 
+            for (int i = 0; i < str.Length; ++i) {
+                switch (str[i]) { 
+                    case 'F':
+                        Point p0 = fractal.Last();
+                        int x, y;
+                        x = p0.X + len;
+                        y = p0.Y;
 
+                        var p = rotation(ang, p0.X, p0.Y, x, y);
+                        fractal.Add(p);
 
-		}
-	}
+                        break;
+                    case '+':
+                        ang = (ang + angle);// % 360; //Может не сработать, но нужно именно такое поведение
+                        break;
+                    case '-':
+                        ang = (ang - angle);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            /*string s = "";
+            for (int j = 0; j < fractal.Count; ++j)
+                s += "(" + fractal[j].X + " ; " + fractal[j].Y + ")";
+            textBox1.Text = s;*/
+        }
+
+        private void render() {
+            int minx, maxx, miny, maxy;
+            IEnumerable<Point> query = fractal.OrderBy(x => x.X);
+            minx = query.First().X;
+            maxx = query.Last().X;
+            query = fractal.OrderBy(y => y.Y);
+            miny = query.First().Y;
+            maxy = query.Last().Y;
+
+            string s = "(" + minx + " ; " + miny + ") " + "(" + maxx + " ; " + maxy + ")";
+          //  textBox1.Text = s;
+
+            fractal = fractal.Select(p => new Point(Convert.ToInt32(pictureBox1.Width * (p.X - minx) / (maxx - minx)), 
+                Convert.ToInt32(pictureBox1.Height*(p.Y - miny) / (maxy - miny)))).ToList();
+
+            g.DrawLines(new Pen(Color.Black), fractal.ToArray());
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cnt = Convert.ToInt32(textBox1.Text);
+            axiom = param[0];  //аксиома
+            rules = new Dictionary<char, string>(); //правила
+            angle = Convert.ToDouble(param[1]); // угол
+            direction = Convert.ToChar(param[2]); // направление
+            for (int i = 3; i < param.Count; ++i){
+                char key = param[i][0];
+                rules.Add(key, param[i].Substring(3));
+            }
+            //Раскрытие фрактала
+            string pred = direction + axiom; //Обрабатывается начальное направление, смотреть внимательней
+            string result = axiom;
+            for (int i = 0; i < cnt; ++i){
+                result = "";
+                for (int j = 0; j < pred.Length; ++j){
+                    string temp = pred[j].ToString();
+                    if (rules.TryGetValue(temp[0], out temp))
+                        result += temp;
+                    else
+                        result += pred[j];
+                }
+                pred = result;
+            }
+            //textBox1.Text = result;
+            perform_fractal(result);
+            render();
+        }
+
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e){
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = pictureBox1.Image;
+        }
+    }
 }
