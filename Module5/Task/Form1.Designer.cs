e04e50c68f9ea5ef26c12bcd85618b6dc8aa3bc8
Ukolnir <metallwind@gmail.com>
2018-10-20 23:25:19 +0300

    Create Module 5
    Сделаны классы для многогранников, реализованы гексаэдр и тетраэдр,
    октаэдр чет не получился

---
+++ Module5/Task/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Task
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(180, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(873, 491);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выбор примитива";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(913, 537);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Гексаэдр",
+            "Тетраэдр",
+            "Октаэдр"});
+            this.comboBox1.Location = new System.Drawing.Point(16, 82);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 46);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Нарисовать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(186, 539);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(348, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1105, 608);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Module5/Task/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bmp = (Bitmap)pictureBox1.Image;
+            Clear();
+            pictureBox1.Image = bmp;
+        }
+
+        public Graphics g;
+        public Bitmap bmp;
+
+        public void Clear() {
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        //Пока рисует объект многогранник (любой из)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Polyhedron pol = new Polyhedron();
+
+            var temp = pol.getEdges(pol.vertices[0]);
+            textBox1.Text = "(" + temp[0].Item1.X + "; " + temp[0].Item1.Y +")   ("+
+                temp[0].Item2.X + "; " + temp[0].Item2.Y +")   ("+
+                temp[1].Item2.X + "; " + temp[1].Item2.Y +")   ("+
+                temp[2].Item2.X + "; " + temp[2].Item2.Y +")";
+
+            switch (comboBox1.SelectedItem.ToString()) { 
+                case "Гексаэдр":
+                   //Уже внутри объекта при инициализации
+                break;
+                case "Тетраэдр":
+                    pol.Tetrahedron();
+                    break;
+                case "Октаэдр":
+                    pol.Octahedron();
+
+                    break;
+                default:
+                    break;
+            }
+            Pen p = new Pen(Color.Black);
+            foreach (var t in pol.vis_edge)
+                g.DrawLine(p, t.Item1, t.Item2);
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        private void button1_Click(object sender, EventArgs e){
+            Clear();
+        }
+    }
+
+
+
+
+    public class Polyhedron
+    {
+        Form1 _form = new Form1(); //Доступ к элементам формы
+        int len = 100;
+        public List<Point> vertices; //Список вершин многогранника
+        public List<Tuple<Point, Point>> vis_edge; //Видимые ребра
+        List<Tuple<Point, Point>> invis_edge; //Невидимые ребра
+        //Конструктор. Рисуется просто гексаэдр, 
+        //обход вершин: A A1 B1 B C C1 D1 D A, где без индексов - основание куба
+
+        public Polyhedron()
+        {
+            //Рисование куба! //Слабонервным не смотреть
+            
+            vis_edge = new List<Tuple<Point, Point>>();
+            vertices = new List<Point>();
+            invis_edge = new List<Tuple<Point, Point>>();
+            //A
+            Point start = new Point(_form.pictureBox1.Width / 2 - 100, _form.pictureBox1.Height / 2);
+            vertices.Add(start);
+            //A1
+            vertices.Add(new Point(start.X, start.Y - len));
+            //A-A1
+            vis_edge.Add(Tuple.Create(start, vertices.Last()));
+            //B1
+            var p = rotation(-45, vertices.Last().X, vertices.Last().Y, vertices.Last().X + len / 2, vertices.Last().Y);
+            vertices.Add(p);
+            //A1-B1
+            vis_edge.Add(Tuple.Create(vertices[vertices.Count - 2], vertices.Last()));
+            //B
+            vertices.Add(new Point(vertices.Last().X, vertices.Last().Y + 100));
+            //B1-B
+            invis_edge.Add(Tuple.Create(vertices[vertices.Count - 2], vertices.Last()));
+            //A-B
+            invis_edge.Add(Tuple.Create(start, vertices.Last()));
+            //C
+            vertices.Add(new Point(vertices.Last().X + len, vertices.Last().Y));
+            //B-C
+            invis_edge.Add(Tuple.Create(vertices[vertices.Count - 2], vertices.Last()));
+            //C1
+            vertices.Add(new Point(vertices.Last().X, vertices.Last().Y - 100));
+            //C-C1
+            vis_edge.Add(Tuple.Create(vertices[vertices.Count - 2], vertices.Last()));
+            //C1-B1
+            vis_edge.Add(Tuple.Create(vertices[2], vertices.Last()));
+            //D1
+            p = rotation(-45, vertices.Last().X, vertices.Last().Y, vertices.Last().X - len / 2, vertices.Last().Y);
+            vertices.Add(p);
+            //C1-D1
+            vis_edge.Add(Tuple.Create(vertices[vertices.Count - 2], vertices.Last()));
+            //A1-D1
+            vis_edge.Add(Tuple.Create(vertices[1], vertices.Last()));
+            //D
+            vertices.Add(new Point(vertices.Last().X, vertices.Last().Y + 100));
+            //D1-D
+            vis_edge.Add(Tuple.Create(vertices[vertices.Count - 2], vertices.Last()));
+            //C-D
+            vis_edge.Add(Tuple.Create(vertices[4], vertices.Last()));
+            //A-D
+            vis_edge.Add(Tuple.Create(start, vertices.Last()));
+        }
+
+        public List<Tuple<Point, Point>> getEdges(Point f) {
+            return vis_edge.Where(x => x.Item1 == f || x.Item2 == f).Union(
+                invis_edge.Where(y => y.Item1 == f || y.Item2 == f)).ToList();
+        }
+
+        private double[,] matrix_multiplication(double[,] m1, double[,] m2)
+        {
+            double[,] res = new double[m1.GetLength(0), m2.GetLength(1)];
+            for (int i = 0; i < m1.GetLength(0); ++i)
+                for (int j = 0; j < m2.GetLength(1); ++j)
+                    for (int k = 0; k < m2.GetLength(0); k++)
+                        res[i, j] += m1[i, k] * m2[k, j];
+            return res;
+        }
+
+        private Point rotation(double ang, int x0, int y0, int x, int y)
+        {
+            double p = ang * Math.PI / 180;
+            double cos = Math.Cos(p);
+            double sin = Math.Sin(p);
+            double[,] transferalMatrix = new double[,] { {cos, sin, 0}, {-sin, cos, 0}, 
+                        {cos*(-x0)+y0*sin+x0, (-x0)*sin-y0*cos+y0, 1}};
+            double[,] point = new double[,] { { x, y, 1.0 } };
+            double[,] res = matrix_multiplication(point, transferalMatrix);
+            return new Point(Convert.ToInt32(res[0, 0]), Convert.ToInt32(res[0, 1]));
+        }
+
+        //Тетраэдр
+        public void Tetrahedron() {
+            List<Point> temp = new List<Point>();
+            for (int i = 0; i < vertices.Count; i += 2)
+                temp.Add(vertices[i]);
+            vertices = temp;
+            vis_edge = new List<Tuple<Point, Point>>();
+            vis_edge.Add(Tuple.Create(vertices[0], vertices[1]));
+            vis_edge.Add(Tuple.Create(vertices[0], vertices[2]));
+            vis_edge.Add(Tuple.Create(vertices[0], vertices[3]));
+            vis_edge.Add(Tuple.Create(vertices[1], vertices[3]));
+            vis_edge.Add(Tuple.Create(vertices[3], vertices[2]));
+            invis_edge = new List<Tuple<Point, Point>>();
+            invis_edge.Add(Tuple.Create(vertices[1], vertices[2]));
+        }
+
+        //Октаэдр
+        public void Octahedron() {
+            var edges = getEdges(vertices.First());
+
+            var pt1 = new Point((edges[1].Item2.X - edges[1].Item1.X) / 2 + edges[1].Item2.X,
+                (edges[0].Item2.Y - edges[0].Item1.Y) / 2 + edges[0].Item2.Y); //(edges[0].Item2.Y - edges[0].Item1.Y) / 2 + edges[0].Item2.Y)
+            var pt2 = new Point((edges[2].Item2.X - edges[2].Item1.X) / 2 + edges[2].Item2.X,
+                (edges[0].Item2.Y - edges[0].Item1.Y) / 2 + edges[0].Item2.Y); //(edges[0].Item2.Y - edges[0].Item1.Y) / 2 + edges[0].Item2.Y)
+            var pt3 = new Point((edges[2].Item2.X - edges[2].Item1.X) / 2 + edges[2].Item2.X,
+                (edges[1].Item2.Y - edges[1].Item1.Y) / 2 + edges[1].Item2.Y); //(edges[1].Item2.Y - edges[1].Item1.Y) / 2 + edges[1].Item2.Y)
+            vis_edge.Clear();
+            vertices.Clear();
+            vertices.Add(pt1); vertices.Add(pt2); vertices.Add(pt3);
+            vis_edge.Add(Tuple.Create(pt1, pt2));
+            vis_edge.Add(Tuple.Create(pt2, pt3));
+            vis_edge.Add(Tuple.Create(pt1, pt3));
+            vertices.Add(new Point(vertices[0].X + len, vertices[0].Y));
+            vis_edge.Add(Tuple.Create(pt2, vertices.Last()));
+            vis_edge.Add(Tuple.Create(pt3, vertices.Last()));
+            var p = rotation(45, pt2.X, pt2.Y, pt2.X + len / 2, pt2.Y);
+            vertices.Add(p);
+            invis_edge.Add(Tuple.Create(vertices[vertices.Count - 2], vertices.Last())); //Для проверки, потом переделать в invis
+            invis_edge.Add(Tuple.Create(pt1, vertices.Last())); //
+            invis_edge.Add(Tuple.Create(pt3, vertices.Last()));   //
+            vertices.Add(new Point(pt3.X, pt3.Y + len));
+            vis_edge.Add(Tuple.Create(pt1, vertices.Last())); 
+            vis_edge.Add(Tuple.Create(pt2, vertices.Last()));
+            vis_edge.Add(Tuple.Create(vertices[3], vertices.Last()));
+        }
+
+        //public virtual void shift() { }
+    }
+}
