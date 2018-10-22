66db55e482020eb8b4d0df403b9e0fb72e2c62b4
BlueMadWolf <sasha_kulishova@mail.ru>
2018-10-22 22:17:23 +0300

    draw point, line, polygons created

--- Module5/Task/Form1.Designer.cs
+++ Module5/Task/Form1.Designer.cs
@@ -34,7 +34,12 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -46,16 +51,18 @@
             this.pictureBox1.Size = new System.Drawing.Size(873, 491);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Location = new System.Drawing.Point(19, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(145, 20);
+            this.label1.Size = new System.Drawing.Size(137, 20);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Выбор примитива";
+            this.label1.Text = "Выбор элемента";
             // 
             // button1
             // 
@@ -71,17 +78,22 @@
             // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
+            "...",
             "Гексаэдр",
             "Тетраэдр",
-            "Октаэдр"});
-            this.comboBox1.Location = new System.Drawing.Point(16, 82);
+            "Октаэдр",
+            "Точка",
+            "Отрезок",
+            "Полигон"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 90);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(140, 21);
             this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(16, 195);
+            this.button2.Location = new System.Drawing.Point(16, 274);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(139, 46);
             this.button2.TabIndex = 4;
@@ -96,11 +108,52 @@
             this.textBox1.Size = new System.Drawing.Size(348, 20);
             this.textBox1.TabIndex = 5;
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(11, 204);
+            this.trackBar1.Maximum = 350;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(163, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseMove);
+            this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseUp);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(85, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Координата z: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(177, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1105, 608);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.comboBox1);
@@ -110,6 +163,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,6 +177,10 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Module5/Task/Form1.cs
+++ Module5/Task/Form1.cs
@@ -12,19 +12,54 @@ namespace Task
 {
     public partial class Form1 : Form
     {
+        PointPol dot = new PointPol(-1, -1, -1);
+        Tuple<PointPol, PointPol> line;
+        List<PointPol> polygon = new List<PointPol>();
+
+        int ind_op;
+
+        public double[,] matrix_multiplication(double[,] m1, double[,] m2)
+        {
+            double[,] res = new double[m1.GetLength(0), m2.GetLength(1)];
+            for (int i = 0; i < m1.GetLength(0); ++i)
+                for (int j = 0; j < m2.GetLength(1); ++j)
+                    for (int k = 0; k < m2.GetLength(0); k++)
+                        res[i, j] += m1[i, k] * m2[k, j];
+            return res;
+        }
+
+
         public Form1()
         {
             InitializeComponent();
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             bmp = (Bitmap)pictureBox1.Image;
             Clear();
+            trackBar1.Value = 0;
+            trackBar1.Maximum = pictureBox1.Width;
             pictureBox1.Image = bmp;
+            textBox2.Text = trackBar1.Value.ToString();
+
         }
 
         public Graphics g;
         public Bitmap bmp;
 
-        public void Clear() {
+        public void Clear()
+        {
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = pictureBox1.Image;
+            label3.Text = "";
+            ind_op = -1;
+            comboBox1.SelectedItem = "...";
+            dot = new PointPol(-1, -1, -1);
+            line = Tuple.Create(dot, dot);
+            polygon.Clear();
+        }
+
+        public void ClearWithout()
+        {
             g = Graphics.FromImage(pictureBox1.Image);
             g.Clear(pictureBox1.BackColor);
             pictureBox1.Image = pictureBox1.Image;
@@ -35,32 +70,187 @@ namespace Task
         {
             Polyhedron pol = new Polyhedron();
 
-            switch (comboBox1.SelectedItem.ToString()) { 
+            switch (comboBox1.SelectedItem.ToString())
+            {
                 case "Гексаэдр":
-                   //Уже внутри объекта при инициализации
-                break;
+                    //Уже внутри объекта при инициализации
+                    break;
                 case "Тетраэдр":
                     pol.Tetrahedron();
                     break;
                 case "Октаэдр":
                     pol.Octahedron();
-
+                    break;
+                case "Точка":
+                    ind_op = 1;
+                    label3.Text = "Выберите координаты точки х и у в окне, а также дополнтельно координату z";
+                    break;
+                case "Отрезок":
+                    ind_op = 2;
+                    label3.Text = "Выберите координаты точки х и у в окне, а также дополнтельно координату z";
+                    break;
+                case "Полигон":
+                    ind_op = 3;
+                    label3.Text = "Выберите координаты точки х и у в окне, а также дополнтельно координату z";
                     break;
                 default:
                     break;
             }
 
-            Pen col = new Pen(Color.Black);
-            pol.Display();
-            for(int i = 0; i < pol.vertices2D.Count; ++i)
-                for (int j = 1; j < pol.vertices2D.Count; ++j)
-                    g.DrawLine(col, pol.vertices2D[i], pol.vertices2D[j]);
-              
+            if (ind_op == -1)
+            {
+                Pen col = new Pen(Color.Black);
+                // pol.Display();
+                for (int i = 0; i < pol.vertices2D.Count; ++i)
+                    for (int j = 1; j < pol.vertices2D.Count; ++j)
+                        g.DrawLine(col, pol.vertices2D[i], pol.vertices2D[j]);
+
+                pictureBox1.Image = pictureBox1.Image;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int v = Int32.Parse(textBox2.Text);
+            trackBar1.Value = v;
+        }
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            double z = trackBar1.Value;
+            dot = new PointPol(e.X, e.Y, z);
+
+            switch (ind_op)
+            {
+                case 1:
+                    break;
+                case 2:
+                    if (line.Item1.X == -1)
+                        line = Tuple.Create(dot, dot);
+                    else
+                        line = Tuple.Create(line.Item1, dot);
+                    break;
+                case 3:
+                    polygon.Add(dot);
+                    break;
+            }
+        }
+
+        public void draw_elems(PointPol p)
+        {
+            double[,] transformMatrix = new double[3, 3] { { Math.Sqrt(0.5), 0, -Math.Sqrt(0.5) }, { 1 / Math.Sqrt(6), Math.Sqrt(2) / 3, 1 / Math.Sqrt(6) }, { 1 / Math.Sqrt(3), -1 / Math.Sqrt(3), 1 / Math.Sqrt(3) } };
+            var temp = matrix_multiplication(transformMatrix, p.getP());
+            Point dot2D = new Point(Convert.ToInt32(temp[0, 0]), Convert.ToInt32(temp[1, 0]));
+            bmp = (Bitmap)pictureBox1.Image;
+            bmp.SetPixel(dot2D.X, dot2D.Y, Color.Black);
+            bmp.SetPixel(dot2D.X + 1, dot2D.Y, Color.Black);
+            bmp.SetPixel(dot2D.X - 1, dot2D.Y, Color.Black);
+            bmp.SetPixel(dot2D.X, dot2D.Y + 1, Color.Black);
+            bmp.SetPixel(dot2D.X, dot2D.Y - 1, Color.Black);
+            pictureBox1.Image = bmp;
+        }
+
+        public void draw_elems(Tuple<PointPol, PointPol> linel)
+        {
+            double[,] transformMatrix = new double[3, 3] { { Math.Sqrt(0.5), 0, -Math.Sqrt(0.5) }, { 1 / Math.Sqrt(6), Math.Sqrt(2) / 3, 1 / Math.Sqrt(6) }, { 1 / Math.Sqrt(3), -1 / Math.Sqrt(3), 1 / Math.Sqrt(3) } };
+            var temp = matrix_multiplication(transformMatrix, linel.Item1.getP());
+            Point p1 = new Point(Convert.ToInt32(temp[0, 0]), Convert.ToInt32(temp[1, 0]));
+
+            var temp1 = matrix_multiplication(transformMatrix, linel.Item2.getP());
+            Point p2 = new Point(Convert.ToInt32(temp1[0, 0]), Convert.ToInt32(temp1[1, 0]));
+
+            if (p1 != p2)
+            {
+                Pen mypen = new Pen(Color.Black);
+                g = Graphics.FromImage(pictureBox1.Image);
+                g.DrawLine(mypen, p1, p2);
+                pictureBox1.Image = pictureBox1.Image;
+            }
+
+
+        }
+
+        public void draw_elems(List<PointPol> pol)
+        {
+            List<Point> polygon2D = new List<Point>();
+
+            foreach (var p in pol)
+            {
+                double[,] transformMatrix = new double[3, 3] { { Math.Sqrt(0.5), 0, -Math.Sqrt(0.5) }, { 1 / Math.Sqrt(6), Math.Sqrt(2) / 3, 1 / Math.Sqrt(6) }, { 1 / Math.Sqrt(3), -1 / Math.Sqrt(3), 1 / Math.Sqrt(3) } };
+                var temp = matrix_multiplication(transformMatrix, p.getP());
+                polygon2D.Add(new Point(Convert.ToInt32(temp[0, 0]), Convert.ToInt32(temp[1, 0])));
+            }
+
+            Point p1 = polygon2D.First();
+            g = Graphics.FromImage(pictureBox1.Image);
+            Pen mypen = new Pen(Color.Black);
+
+            for (int i = 1; i < polygon2D.Count(); i++)
+            {
+                g.DrawLine(mypen, p1, polygon2D[i]);
+                p1 = polygon2D[i];
+            }
+
             pictureBox1.Image = pictureBox1.Image;
         }
 
-        private void button1_Click(object sender, EventArgs e){
-            Clear();
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            double z = trackBar1.Value;
+
+            switch (ind_op)
+            {
+                case 1:
+                    draw_elems(dot);
+                    break;
+                case 2:
+                    //line = Tuple.Create(dot, new PointPol(e.X, e.Y, z));
+                    draw_elems(line);
+                    break;
+                case 3:
+                    polygon.Add(new PointPol(e.X, e.Y, z));
+                    draw_elems(polygon);
+                    break;
+            }
+
+            label3.Text = "";
+        }
+
+        private void trackBar1_MouseUp(object sender, MouseEventArgs e)
+        {
+            textBox2.Text = trackBar1.Value.ToString();
+        }
+
+        private void trackBar1_MouseMove(object sender, MouseEventArgs e)
+        {
+            textBox2.Text = trackBar1.Value.ToString();
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Точка":
+                    ind_op = 1;
+                    label3.Text = "Выберите координаты точки х и у в окне, а также дополнтельно координату z";
+                    break;
+                case "Отрезок":
+                    ind_op = 2;
+                    label3.Text = "Выберите координаты точки х и у в окне, а также дополнтельно координату z";
+                    break;
+                case "Полигон":
+                    ind_op = 3;
+                    label3.Text = "Выберите координаты точки х и у в окне, а также дополнтельно координату z";
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
@@ -148,16 +338,7 @@ namespace Task
 
         }
 
-        private double[,] matrix_multiplication(double[,] m1, double[,] m2)
-        {
-            double[,] res = new double[m1.GetLength(0), m2.GetLength(1)];
-            for (int i = 0; i < m1.GetLength(0); ++i)
-                for (int j = 0; j < m2.GetLength(1); ++j)
-                    for (int k = 0; k < m2.GetLength(0); k++)
-                        res[i, j] += m1[i, k] * m2[k, j];
-            return res;
-        }
-
+        /*
         private Point rotation(double ang, int x0, int y0, int x, int y)
         {
             double p = ang * Math.PI / 180;
@@ -166,10 +347,10 @@ namespace Task
             double[,] transferalMatrix = new double[,] { {cos, sin, 0}, {-sin, cos, 0}, 
                         {cos*(-x0)+y0*sin+x0, (-x0)*sin-y0*cos+y0, 1}};
             double[,] point = new double[,] { { x, y, 1.0 } };
-            double[,] res = matrix_multiplication(point, transferalMatrix);
+           double[,] res = matrix_multiplication(point, transferalMatrix);
             return new Point(Convert.ToInt32(res[0, 0]), Convert.ToInt32(res[0, 1]));
         }
-
+        */
         //Тетраэдр
         public void Tetrahedron() {
 
@@ -180,6 +361,7 @@ namespace Task
             
         }
 
+        /*
         public void Display() { 
             vertices2D = new List<Point>();
             double[,] transformMatrix = new double[3,3]{{Math.Sqrt(0.5),0,-Math.Sqrt(0.5)},{1/Math.Sqrt(6),Math.Sqrt(2)/3,1/Math.Sqrt(6)},{1/Math.Sqrt(3), -1/Math.Sqrt(3), 1/Math.Sqrt(3)}};
@@ -189,7 +371,7 @@ namespace Task
                 
             }
         } 
-
+        */
         //public virtual void shift() { }
     }
 }
