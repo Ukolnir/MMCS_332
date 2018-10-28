28dfece72400a4931c91d9153df1217e4542f57e
Ukolnir <metallwind@gmail.com>
2018-10-28 16:29:09 +0300

    UPD Union

---
+++ UnionPolygons/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace UnionPolygons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(116, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(844, 494);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown1);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.FullOpen = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить полигон";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Объединить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 546);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ UnionPolygons/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnionPolygons
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bmp = (Bitmap)pictureBox1.Image;
+            Clear();
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+        }
+
+        private void Clear()
+        {
+            var g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        List<Point> general = new List<Point>();
+        List<Point> newPolygon = new List<Point>();
+        bool flag = false;
+        bool twoPolygons = false;
+
+        Graphics g;
+        private void pictureBox1_MouseDown1(object sender, MouseEventArgs e)
+        {
+            if (!twoPolygons){
+                if (flag){
+                    newPolygon.Add(new Point(e.X, e.Y));
+                    g.DrawLines(new Pen(Color.Black), newPolygon.ToArray());
+                }
+                else
+                {
+                    newPolygon.Add(new Point(e.X, e.Y));
+                    flag = true;
+                }
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (twoPolygons){
+                label1.Text = "В данной реализации объединение работает только с двумя полигонами одновременно";
+                return;
+            }
+
+            if (newPolygon.First() != newPolygon.Last()){
+                newPolygon.Add(newPolygon.First());
+                g.DrawLines(new Pen(Color.Black), newPolygon.ToArray());
+                pictureBox1.Image = bmp;
+            }
+
+            if (general.Count == 0){
+                general = newPolygon;
+                newPolygon.Clear();
+                flag = false;
+            }
+            else{
+                twoPolygons = true;
+                label1.Text = "Объединение данной реализации работает только с двумя полигонами одновременно";
+            }
+        }
+    }
+}
