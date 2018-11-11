d8815ea5252d5d78d1bed58ef0000fce80fc6328
Ukolnir <metallwind@gmail.com>
2018-11-11 15:54:50 +0300

    add assembly
    перевод (для Ukolnir)

---
+++ Module6/assembly/Polygon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public class Polygon
+    {
+        public List<int> vertices;
+        public List<Edge> edges;
+
+        public Polygon(List<int> inp) {
+            vertices = inp;
+            edges = new List<Edge>();
+            for (int i = 0; i < inp.Count; ++i)
+                edges.Add(new Edge(inp[i], inp[(i + 1) % inp.Count]));
+        }
+
+        public string checkin()
+        {
+            string res = " Ребра: ";
+            foreach (var i in edges)
+                res += "(" + i.E1 + ";" + i.E2 + ")   "; 
+            return res;
+        }
+    }
+
+    public class Edge {
+        public int E1, E2;
+        public Edge(int e1, int e2) {
+            E1 = e1; E2 = e2;
+        }    
+    }
+
+}
