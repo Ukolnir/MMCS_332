8f37a57a46f3524a4d592f5cf99e1ba1d70609f2
BlueMadWolf <sasha_kulishova@mail.ru>
2018-11-25 00:04:55 +0300

    Module 10 upd.

--- Module10/Cars/main.cpp
+++ Module10/Cars/main.cpp
@@ -1,6 +1,7 @@
-#define _CRT_SECURE_NO_WARNINGS
+
 
 #include <iostream>
+#include <Windows.h>
 #include "D:\Документы\OneDrive\Документы\7 семестр\комп. графика\freeglut\include\GL\freeglut.h"
 #include "D:\Документы\OneDrive\Документы\7 семестр\комп. графика\freeglut\include\GL\SOIL.h"
 
@@ -9,35 +10,72 @@ unsigned int photo_tex;
 int w, h;
 
 void LoadImage() {
-
+	texture = SOIL_load_OGL_texture("D:/Games/road.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT);
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
 }
 
+/*#define checkImageWidth 64 
+#define checkImageHeight 64 
+GLubyte checkImage[checkImageHeight][checkImageWidth][4];
+GLuint texName;
+void makeCheckImage()
+{
+	int i, j, c;
+	for (i = 0; i<checkImageHeight; i++)
+	{
+		for (j = 0; j<checkImageWidth; j++)
+		{
+			c = (((i & 0x8) == 0) ^ ((j & 0x8) == 0)) * 255;
+			checkImage[i][j][0] = (GLubyte)c;
+			checkImage[i][j][1] = (GLubyte)c;
+			checkImage[i][j][2] = (GLubyte)c;
+			checkImage[i][j][3] = (GLubyte)255;
+		}
+	}
 
+}
+*/
 void Init(void) {
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	/*makeCheckImage();
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glGenTextures(1, &texName);
+	glBindTexture(GL_TEXTURE_2D, texName);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth, checkImageHeight,
+		0, GL_RGBA, GL_UNSIGNED_BYTE, checkImage);
+		*/
 }
 
 void Update(void) {
-	//LoadImage();
 	double angle = 1;
 	angle += 0.5f;
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	gluLookAt(100.0f, 100.0f, 100.0f, 60.0f, 50.0f, 40.0f, 0.0f, 1.0f, 0.0f);
 	glRotatef(angle, 0.0f, 1.0f, 0.0f);
 
-	texture = SOIL_load_OGL_texture("road.bmp", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_INVERT_Y);
-	glGenTextures(1, &texture);
+	LoadImage();
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	
+	glEnable(GL_TEXTURE_2D);
+
 	glBegin(GL_POLYGON);
-	glTexCoord2d(0, 0); glVertex3d(0, 0, 0);
-	glTexCoord2d(1, 0); glVertex3d(320, 0, 0);
-	glTexCoord2d(1, 1); glVertex3d(320, 0, 320);
-	glTexCoord2d(0, 1); glVertex3d(0, 0, 320);
+	glTexCoord2f(0, 0); glVertex3f(0, 0, 0);
+	glTexCoord2f(0, 1); glVertex3f(0, 0, 320);
+	glTexCoord2f(1, 1); glVertex3f(320, 0, 320);
+	glTexCoord2f(1, 0); glVertex3f(320, 0, 0);
 	glEnd();
-	glFlush();
 	glDisable(GL_TEXTURE_2D);
+
+	/*glColor3f(1, 0.2, 0.15);
+	glutSolidCube(20);*/
+	glFlush();
+	glutSwapBuffers();
+	
 }
 //Функця вызываемая при изменении размеров окна
 void Reshape(int width, int height) {
