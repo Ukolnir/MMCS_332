f7a79082fee8e050e8604fe7974a975582f87237
Ukolnir <user@mil8a-202a-03.mmcs.sfedu.ru>
2018-12-04 16:49:13 +0300

    Module 09 task 2 done

--- Module09/Task 2/main.cpp
+++ Module09/Task 2/main.cpp
@@ -15,10 +15,8 @@ void Init(void) {
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_COLOR_MATERIAL);
-
 	glEnable(GL_LIGHT0);
 	glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
-
 	glEnable(GL_NORMALIZE);
 }
 
@@ -31,86 +29,99 @@ void Reshape(int width, int height) {
 	width = w; height = h;
 }
 
+//first
+float x = 4.5, y = 1.8, z = 0;
+//second
+float x1 = 6.0, y1 = -1.0, z1 = 0;
+//third
+float x2 = 3.0, y2 = -1.0, z2 = 0;
+
+//center
+float o1 = 4.5, o2 = 0, o3 = 0;
+
 void drawWireCube() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	if(!projection){
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		glOrtho(-1, 1, -1, 1, -1, 1);
+		glOrtho(-10, 10, -10, 10, -10, 10);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 	}
 	else {
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		gluPerspective(65.0f, w / h, 1.0f, 1000.0f);
+		gluPerspective(65.0f, w / h, 0.1f, 1000.0f);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
-		gluLookAt(-0.3, 0.5, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+		gluLookAt(0.0f, 0.0f, 15.0f, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 	}
 	
-	glLoadIdentity();
+	glPushMatrix();
 	
 	if(!mode)
-		glTranslated(0.45, 0.18, 0);
-	if(mode ==2)
-		glTranslated(0.45, 0.18, 0);
+		glTranslated(x, y, z);
+	if (mode == 2)
+		glTranslated(o1, o2, o3);
 
 	glRotatef(rotate_x, 1.0, 0.0, 0.0);
 	glRotatef(rotate_y, 0.0, 1.0, 0.0);
 	glRotatef(rotate_z, 0.0, 0.0, 1.0);
 
 	if(mode==1)
-		glTranslated(0.45, 0.18, 0);
+		glTranslated(x, y, z);
 
 	if (mode == 2) {
-		glTranslated(-0.45, -0.18, 0);
-		glTranslated(0.45, 0.18, 0);
+		glTranslated(-o1, -o2, -o3);
+		glTranslated(x, y, z);
 	}
-	glColor3f(238/255.0, 201/255.0, 0);
-	glutSolidCube(0.2);
 
-	glLoadIdentity();
+	glColor3f(238/255.0, 201/255.0, 0);
+	glutSolidCube(2);
+
+	glPopMatrix();
+	glPushMatrix();
 	if (!mode)
-		glTranslated(0.6, -0.1, 0);
+		glTranslated(x1, y1, z1);
 	if (mode == 2)
-		glTranslated(0.45, 0.18, 0);
+		glTranslated(o1, o2, o3);
 
 	glRotatef(rotate_x, 1.0, 0.0, 0.0);
 	glRotatef(rotate_y, 0.0, 1.0, 0.0);
 	glRotatef(rotate_z, 0.0, 0.0, 1.0);
 
 	if (mode == 1)
-		glTranslated(0.6, -0.1, 0);
+		glTranslated(x1, y1, z1);
 	if (mode == 2) {
-		glTranslated(-0.45, -0.18, 0);
-		glTranslated(0.6, -0.1, 0);
+		glTranslated(-o1, -o2, -o3);
+		glTranslated(x1, y1, z1);
 	}
 
 	glColor3f(184 / 255.0, 134 / 255.0, 11 / 255.0);
-	glutSolidCube(0.2);
+	glutSolidCube(2);
 
-	glLoadIdentity();
+	glPopMatrix();
+	glPushMatrix();
 	
 	if (!mode) 
-		glTranslated(0.3, -0.1, 0);
+		glTranslated(x2, y2, z2);
 	if (mode == 2)
-		glTranslated(0.45, 0.18, 0);
+		glTranslated(o1, o2, o3);
 
 	glRotatef(rotate_x, 1.0, 0.0, 0.0);
 	glRotatef(rotate_y, 0.0, 1.0, 0.0);
 	glRotatef(rotate_z, 0.0, 0.0, 1.0);
 
 	if (mode == 1) 
-		glTranslated(0.3, -0.1, 0);
+		glTranslated(x2, y2, z2);
 
 	if (mode == 2) {
-		glTranslated(-0.45, -0.18, 0);
-		glTranslated(0.3, -0.1, 0);
+		glTranslated(-o1, -o2, -o3);
+		glTranslated(x2, y2, z2);
 	}
 	glColor3f(0.75, 0.75, 0.75);
-	glutSolidCube(0.2);
-
+	glutSolidCube(2);
+	
 	glFlush();
 	glutSwapBuffers();
 }
@@ -134,7 +145,7 @@ void specialKeys(int key, int x, int y) {
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitWindowPosition(500, 0);
+	glutInitWindowPosition(250, 0);
 	glutInitWindowSize(w, h);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 	glutCreateWindow("OpenGL");
