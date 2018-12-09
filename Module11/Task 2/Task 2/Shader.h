cb39edbe6ebe9852d22ee1624f995c20f8e8f595
Ukolnir <metallwind@gmail.com>
2018-12-09 16:29:38 +0300

    Добавлены библиотеки
    + попытка работы с VBO

---
+++ Module11/Task 2/Task 2/Shader.h
@@ -0,0 +1,13 @@
+#include "glm.hpp"
+#include <GL\glew.h>
+#include <GL\freeglut.h>
+
+using namespace glm;
+
+class Shader {
+	GLuint ShaderProgram;
+	GLuint vertex_shader;
+	GLuint fragment_shader;
+
+
+};
