3dc536d0d9fe9a5bbbe50477cf359a288768d54a
BlueMadWolf <sasha_kulishova@mail.ru>
2018-12-10 22:59:36 +0300

    opengl with bugs ==@

---
+++ Module11/Task1/main.cpp
@@ -0,0 +1,141 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <Windows.h>
+#include "D:\Документы\OneDrive\Документы\7 семестр\комп. графика\freeglut\include\GL\glew.h"
+#include "D:\Документы\OneDrive\Документы\7 семестр\комп. графика\freeglut\include\GL\wglew.h"
+#include "D:\Документы\OneDrive\Документы\7 семестр\комп. графика\freeglut\include\GL\freeglut.h"
+#include "D:\Документы\OneDrive\Документы\7 семестр\комп. графика\freeglut\include\GL\SOIL.h"
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+vector<GLfloat> vertices;
+vector<GLushort> elements;
+int w, h;
+GLuint VBO, VBI;
+
+
+void checkOpenGLerror()
+{
+	GLenum errCode;
+	if ((errCode = glGetError()) != GL_NO_ERROR)
+		std::cout << "OpenGl error! - " << gluErrorString(errCode);
+}
+
+void createBuffers()
+{
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER,
+		vertices.size() * sizeof(GLfloat),
+		vertices.data(),
+		GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glGenBuffers(1, &VBI);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBI);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+		elements.size() * sizeof(GLushort),
+		elements.data(),
+		GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	checkOpenGLerror();
+}
+
+
+void loadOBJ(string path)
+{
+	ifstream in(path, ios::in);
+	if (!in)
+	{
+		cerr << "Cannot open " << "sample.obj" << endl; exit(1);
+	}
+
+	string line;
+	while (getline(in, line))
+	{
+		if (line.substr(0, 2) == "v ")
+		{
+			istringstream s(line.substr(2));
+			GLfloat v;
+			s >> v; vertices.push_back(v);
+			s >> v; vertices.push_back(v);
+			s >> v; vertices.push_back(v);
+
+		}
+		else if (line.substr(0, 2) == "f ")
+		{
+			istringstream s(line.substr(2));
+			GLushort a, b, c;
+			s >> a; s >> b; s >> c;
+			elements.push_back(a); elements.push_back(b); elements.push_back(c);
+		}
+	}
+}
+
+void Init(void) {
+	loadOBJ("vase.obj");
+	createBuffers();
+}
+
+
+void DrawVBO()
+{
+	//GLvoid * p;// = new Glvoid[0];
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexPointer(3, GL_FLOAT, 0, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBI);
+	glDrawElements(GL_TRIANGLES, elements.size(), GL_UNSIGNED_BYTE, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+void Update(void) {
+	
+	//createBuffers();
+	//DrawVBO();
+	glFlush();
+	glutSwapBuffers();
+
+
+}
+//Функця вызываемая при изменении размеров окна
+void Reshape(int width, int height) {
+	w = width;
+	h = height;
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(65.0f, w / h, 1.0f, 1000.0f);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+int main(int argc, char ** argv) {
+	glutInit(&argc, argv);
+	glutInitWindowPosition(250, 0);
+	glutInitWindowSize(w, h);
+	
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("OpenGL");
+	glutReshapeFunc(Reshape);
+	glutDisplayFunc(DrawVBO);
+
+	Init();
+
+	glutMainLoop();
+
+//	glutMainLoop();
+	return 0;
+}
